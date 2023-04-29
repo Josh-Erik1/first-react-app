cde9975b2620c882671bd731e6015384733d045f
Josh-Erik1 <jseriki21@gmail.com>
2023-04-29 14:00:12 +0100

    Navbar added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,9 @@ import { Inter } from "next/font/google";
 import Head from "next/head";
 import { Container } from "react-bootstrap";
 import styles from "@/styles/App.module.css";
+import NextNProgress from "nextjs-progressbar";
+
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +20,8 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <NextNProgress />
+      <Navbar />
 
       <Container className={styles.pageContainer}>
         <Component {...pageProps} />
